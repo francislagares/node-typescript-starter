cd8c0d8c13028f62a297fab611b0a479cb1f7bec
Francis Lagares <francis.lagares@gmail.com>
2023-05-11 16:47:56 +0200

    chore: add pm2 process management

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -27,13 +27,13 @@ module.exports = {
     },
     {
       name: 'dev', // pm2 start App name
-      script: 'ts-node', // ts-node
+      script: './node_modules/.bin/ts-node', // ts-node
       args: '-r tsconfig-paths/register --transpile-only src/server.ts', // ts-node args
       exec_mode: 'cluster', // 'cluster' or 'fork'
       instance_var: 'INSTANCE_ID', // instance variable
-      instances: 2, // pm2 instance count
+      instances: 1, // pm2 instance count
       autorestart: true, // auto restart if process crash
-      watch: false, // files change automatic restart
+      watch: true, // files change automatic restart
       ignore_watch: ['node_modules', 'logs'], // ignore files change
       max_memory_restart: '1G', // restart if process use more than 1G memory
       merge_logs: true, // if true, stdout and stderr will be merged and sent to pm2 log
