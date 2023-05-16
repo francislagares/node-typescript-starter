1293d292a76690cf809ed8c7fcdaabaf87bae719
Francis Lagares <francis.lagares@gmail.com>
2023-05-16 18:39:38 +0200

    chore: add vite config

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import path from 'path';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  plugins: [viteTsconfigPaths()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 5173, // you can replace this port with any port
+  },
+  test: {
+    globals: true,
+    environment: 'node',
+    coverage: {
+      reporter: ['text', 'html'],
+      exclude: ['node_modules/'],
+    },
+  },
+  resolve: {
+    alias: {
+      '@/src': path.resolve(__dirname, './src'),
+    },
+  },
+});
